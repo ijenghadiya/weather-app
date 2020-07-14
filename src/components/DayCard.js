45249065b859586a6f8c5d2aabed6458053461b4
Ijen <ghadiyaijen@gmail.com>
2020-07-14 22:27:55 +0530

    created weekcontainer component

---
+++ src/components/DayCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DayCard extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>Hi</h1> 
+            </div>
+        )
+    }
+
+}
+export default DayCard;
